ecd7ca23ba93971a3ff4bf09a020df805ee70d00
DESKTOP-6QRP1CK\kiich <kiichi.kendo@gmail.com>
2023-08-02 16:11:35 +0900

    Spinnerを入れた

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -15,28 +15,42 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //全てのID読み取り
-        val MassOfStd:EditText = findViewById(R.id.MassOfStd)
-        val PeakOfStd:EditText = findViewById(R.id.PeakOfStd)
-        val MwOfCom:EditText = findViewById(R.id.MwOfCom)
-        val PeakOfCom:EditText = findViewById(R.id.PeakOfCom)
-        val MassOfCom:EditText = findViewById(R.id.MassOfCom)
-        val NumberOfSpicificProton:EditText = findViewById(R.id.NumberOfSpicificProton)
-        val spinner : Spinner= findViewById(R.id.spinner)
+        val MassOfStd: EditText = findViewById(R.id.MassOfStd)
+        val PeakOfStd: EditText = findViewById(R.id.PeakOfStd)
+        val MwOfCom: EditText = findViewById(R.id.MwOfCom)
+        val PeakOfCom: EditText = findViewById(R.id.PeakOfCom)
+        val MassOfCom: EditText = findViewById(R.id.MassOfCom)
+        val NumberOfSpicificProton: EditText = findViewById(R.id.NumberOfSpicificProton)
+        val spinner: Spinner = findViewById(R.id.spinner)
 
-
-
-        val spinnerAdapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_item)
+        val spinnerAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item)
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        Lang.values().forEach {
+            spinnerAdapter.add(it.name)
         }
-    enum class Lang {
-        KOTLIN,
-        SWIFT,
-        DART
-        }
+        spinner.adapter = spinnerAdapter
+        spinner.onItemSelectedListener = spinnerAdapterListener
+    }
+
+
 
     private val spinnerAdapterListener = object : AdapterView.OnItemSelectedListener {
-        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        override fun onItemSelected(
+            parent: AdapterView<*>?,
+            view: View?,
+            position: Int,
+            id: Long
+        ) {
             // 選択された時に実行したい処理
         }
 
-
+        override fun onNothingSelected(parent: AdapterView<*>?) {
+            // 選択されなかった時に実行したい処理
+        }
     }
+
+    enum class Lang{
+        DSS,
+        DMS
+    }
+}
